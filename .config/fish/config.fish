c600970d671c9ef0a90c5c16ae12b58fbac7df99
opp <>
2023-07-09 21:46:42 -0400

    fish alias add and prompt edit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ alias cl="clear"
 alias py="python3"
 alias cat="bat" # https://github.com/sharkdp/bat
 alias ls="exa" # https://github.com/ogham/exa
-alias l="exa -la"
+alias l="exa -laa"
 alias ll="exa -l"
 alias lt="exa -laT"
 alias rm="rm -iv"
@@ -13,6 +13,7 @@ alias mv="mv -iv"
 alias et="exiftool" # https://www.exiftool.org/install.html
 alias open="xdg-open"
 alias pvt="fish --private"
+alias v="nvim"
 
 # python aliases
 alias venv-create="python3 -m venv .venv"
