f554761e5fb8fe56e2774ab4522186435378f16b
opp <49080343+opp@users.noreply.github.com>
2024-12-05 20:13:08 -0500

    today was a good day

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,10 @@
 alias et="exiftool" # https://www.exiftool.org/install.html
 alias cat="bat" # https://github.com/sharkdp/bat
 alias ls="lsd" # https://github.com/lsd-rs/lsd
-alias l="lsd -l"
-alias la="lsd -la"
+alias l="lsd -la"
+alias ll="lsd -l"
 alias lt="lsd --tree"
 alias rm="rm -iv"
 alias cp="cp -v"
 alias mv="mv -iv"
 alias open="xdg-open"
-
-set -g fish_greeting
